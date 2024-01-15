81ad5f1ce1b811a9b5689280008e3d3a8339027c
yevheniiskakun <yevhenii.skakun@gmail.com>
2024-01-15 15:40:30 +0100

    Added checkbox functionality

--- productlist/pages/urls.py
+++ productlist/pages/urls.py
@@ -10,7 +10,7 @@ app_name = 'pages'
 urlpatterns = [
 	# main view
 	path('', views.index, name='home'),
-	path('add_product/<str>', views.add_product, name='add_product'),
+	path('add_product', views.add_product, name='add_product'),
 	path('delete_product/<int:id>', views.delete_product, name='delete_product'),
-	path('mark_product/<int>', views.mark_product, name='mark_product'),
+	path('mark_product/<int:id>', views.mark_product, name='mark_product'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- productlist/pages/views.py
+++ productlist/pages/views.py
@@ -9,7 +9,21 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def add_product(request):
-    context = {}
+    if request.method == 'POST':
+        print(request.POST.get('new_product'))
+        '''
+        form = MyForm(request.POST)
+
+        print(form['my_field'].value())
+        print(form.data['my_field'])
+
+        if form.is_valid():
+            print(form.cleaned_data['my_field'])
+            print(form.instance.my_field)
+            form.save()
+        '''
+    products = Product.objects.all()
+    context = {"products": products}
     return render(request, 'pages/index.html', context)
 
 def delete_product(request, id):
@@ -19,6 +33,13 @@ def delete_product(request, id):
     return render(request, 'pages/index.html', context)
 
 
-def mark_product(request):
-    context = {}
+def mark_product(request, id):
+    modified_product = Product.objects.get(id=id)
+    if modified_product.buyed == False:
+        modified_product.buyed = True  # change field
+    else:
+        modified_product.buyed = False  # change field
+    modified_product.save()  # this will update only
+    products = Product.objects.all()
+    context = {"products": products}
     return render(request, 'pages/index.html', context)
